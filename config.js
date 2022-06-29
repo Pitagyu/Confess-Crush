69f6eab0c185b62da65b6262546434acba59a067
Pitagyu <108416220+Pitagyu@users.noreply.github.com>
2022-06-29 22:12:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    titleWeb: "HPBD my friend",
+    introTitle: 'Thao cute:3',
+    introDesc: `Chúc mừng bạn tui
+                Đủ tuổi ngồi tù:))
+                Đủ tuổi lấy bằng lái xe
+                Đủ tuổi LẤY CK ^^ `,
+    btnIntro: 'V: PI :V',
+    title: 'M yêu t hay chồng jimin,jihoon,sungwoon.. và 7749 anh khác?',
+    desc: 'Thật lòng nào:) ',
+    btnYes: 'Of course only u( ˘ ³˘)♥',
+    btnNo: 'Ck t cẻm ơn',
+    question: 'Vì t cute đúng hơm¯\_(ツ)_/¯ ',
+    btnReply: '=)))))))',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Thi tốt nha con giời',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
